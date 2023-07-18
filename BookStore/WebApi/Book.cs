dda87646a538089ab6f0ca8d407949d0276fcf4f
Merve Karataş <mervekaratas_1999@hotmail.com>
2023-07-18 11:54:09 +0300

    First_Week_Homework

---
+++ BookStore/WebApi/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApi
+{
+    public class Book
+    {
+        public int Id {  get; set; }   
+        public int GenreId { get; set; } 
+        
+        public int PageCount
+    }
+}
---
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("[controller]s")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+    }
+}
